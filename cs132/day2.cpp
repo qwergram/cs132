121c10b5ef954495780ee782426a06a89de0d2d1
Norton Pengra <npengra317@gmail.com>
2017-01-04 09:54:46 -0800

    cin.get() added

--- cs132/day2.cpp
+++ cs132/day2.cpp
@@ -1,10 +1,16 @@
 #include <iostream> // C++ cin/cout
-#include <iomanip>
+#include <iomanip>  // <io_manip>ulation
 #include <cstdlib>  // includes "stdlib.h"
 
 using namespace std;
 
+// if you write more "main" functions, it will become mangled
+// remember when you tried to extend python w/ c++? yeah we ran into that
+// problem.
 int main() {
-	cout << "hello world!" << endl;
+	// return type is an "ostream"
+	// cout is an ostream, and "<<" is throwing things in regardless of type
+	cout << "hello, world" << '!' << endl;
+	cin.get();
 	return EXIT_SUCCESS;
 }
