d93113618bb52926a24f8124b8422a258296d77e
Norton Pengra <npengra317@gmail.com>
2017-01-04 09:58:25 -0800

    added a cast

--- cs132/day2.cpp
+++ cs132/day2.cpp
@@ -8,9 +8,12 @@ using namespace std;
 // remember when you tried to extend python w/ c++? yeah we ran into that
 // problem.
 int main() {
+	int age = 19;
 	// return type is an "ostream"
 	// cout is an ostream, and "<<" is throwing things in regardless of type
-	cout << "hello, world" << '!' << endl;
+	cout << "hello, world" << '!' << " I am " << age << " years old!" << endl;
+	// This is a cast
+	cout << (int)'N' << endl;
 	cin.get();
 	return EXIT_SUCCESS;
 }
